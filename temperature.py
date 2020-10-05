75af6ac17e84cf00922023891eeaa17aa72fd4ab
Mayank Goyal <65842833+Pixel-Tech@users.noreply.github.com>
2020-10-05 21:49:04 +0530

    Update temperature.py
    Improvised the code and added more conversions.

--- temperature.py
+++ temperature.py
@@ -4,13 +4,36 @@ Created on Fri Jul 12 05:03:02 2019
 
 @author: aot
 """
-c=input("Enter the choice(C or F):")
-temp=int(input("Enter the temperature:"))
-if(c=="C"):
-    result=((9*temp)/5)+32
-    print("The temperature is ={} F".format(result))
-elif(c=="F"):
-    result=((temp-32)*5)/9
-    print("The temperature is ={} C".format(result))
+
+unit1=str(input("Enter the unit of the temperature (C/K/F)="))
+temp=float(input("Enter the temperature:"))
+
+if(unit1=="K"):
+    c=str(input("Enter the unit of temperature you want to convert to (C/F)="))
+    if (c=="C"):
+        result= temp-273.15
+        print("The temperature in Celsius is ",result)
+    elif (c=="F"):
+        result= (temp*(9/5))-459.67
+        print("The temperature in Fahrenheit is ",result)
+
+
+elif(unit1=="C"):
+    c=str(input("Enter the unit of temperature you want to convert to (K/F)="))
+    if (c=="K"):
+        result= temp+273.15
+        print("The temperature in Celsius is ",result)
+    elif (c=="F"):
+        result= (temp*(9/5))+32
+        print("The temperature in Fahrenheit is ",result)
+
+elif(unit1=="F"):
+    c=str(input("Enter the unit of temperature you want to convert to (K/C)="))
+    if (c=="K"):
+        result= ((temp-32)*(5/9))+273.15
+        print("The temperature in Kelvin is ",result)
+    elif (c=="C"):
+        result= (temp-32)*(5/9)
+        print("The temperature in Celsius is ",result)
 else:
-    print("Invalid")
+    print("Invalid entry, please try again !")
